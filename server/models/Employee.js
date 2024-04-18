65e2926433dac6397e59541b49f3bc132b61861b
MabuzaM <mabuzamfano@gmail.com>
2024-04-18 21:16:12 +0200

    started fixing the schema

--- server/models/Employee.js
+++ server/models/Employee.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
 
 const EmployeeSchema = new mongoose.Schema({
-  employeeName: String,
-  employeeSurname: String,
-  employeeDateOfBirth: String, 
-  employeeNatId: String, 
+  employeeName: {
+    type: String,
+    required: true,
+    length: 30,
+  },
+  employeeSurname: {
+    type: String,
+    required: true,
+  },
+  employeeDateOfBirth: {
+    type: DataTransfer,
+    required: true,
+  }, 
+  employeeNatId: string, 
   employeeGender: String,
   employeeMaritalStatus: String,
   employeePhone: String, 
